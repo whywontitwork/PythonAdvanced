559844c8d0f59c555b7602ad92d765b191d64a4d
uyh40101 <33301780+uyh40101@users.noreply.github.com>
2020-03-02 18:50:41 +0900

    Add files via upload

---
+++ 05. Input Function/Practice_02.py
@@ -0,0 +1,11 @@
+# Salary
+
+salary = int(input("월급: "))
+
+getSalary = salary - (salary * 0.033)
+yearSalary = salary * 12
+getYearSalary = getSalary * 12
+
+print(getSalary)
+print(yearSalary)
+print(getYearSalary)
---
+++ 05. Input Function/Practice_03.py
@@ -0,0 +1,7 @@
+# usd
+
+usd = int(input("$: "))
+krw = usd * 1210.50
+
+print("달러: %d" % usd)
+print("원화: %d" % krw)
